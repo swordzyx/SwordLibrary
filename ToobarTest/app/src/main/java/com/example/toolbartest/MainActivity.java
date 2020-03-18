9c0e880b328952d641cbcf43b21e1269e3bbb4ea
zerolans <1163201968@qq.com>
2020-03-18 22:58:30 +0800

    wanandroid toolbar

--- ToobarTest/app/src/main/java/com/example/toolbartest/MainActivity.java
+++ ToobarTest/app/src/main/java/com/example/toolbartest/MainActivity.java
@@ -1,15 +1,22 @@
-package com.example.wanandroid;
+package com.example.toolbartest;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
     }
 }
