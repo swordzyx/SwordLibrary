93113253b9a5df4b9413cd76c87577bdf9dd58f3
zero <1163201968@qq.com>
2020-03-30 20:09:24 +0800

    创建数据库

--- ContentProviderTest/app/src/main/java/blog/zero/com/contentprovidertest/provider/TestContentProvider.java
+++ ContentProviderTest/app/src/main/java/blog/zero/com/contentprovidertest/provider/TestContentProvider.java
@@ -4,14 +4,19 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import blog.zero.com.contentprovidertest.db.TestContract;
+import blog.zero.com.contentprovidertest.db.TestDatabase;
+
 public class TestContentProvider extends ContentProvider {
     final static String AUTHORITY = "blog.zero.com.contentprovidertest.provider";
     final static UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+    private TestDatabase testDatabaseHelper;
 
     static {
         matcher.addURI(AUTHORITY, "table1", 1);
@@ -20,7 +25,8 @@ public class TestContentProvider extends ContentProvider {
 
     @Override
     public boolean onCreate() {
-        return false;
+        testDatabaseHelper = new TestDatabase(getContext());
+        return true;
     }
 
     @Nullable
@@ -28,6 +34,7 @@ public class TestContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
         switch (matcher.match(uri)){
             case 1:
+
                 break;
             case 2:
                 break;
@@ -44,11 +51,36 @@ public class TestContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        final SQLiteDatabase database = testDatabaseHelper.getWritableDatabase();
+        //database must not be null
+        assert database != null;
+        Uri result = null;
+        switch (matcher.match(uri)){
+            case 1:
+                long id = database.insertOrThrow(TestContract.TABLE1.TABLE_NAME, null, values);
+                result = Uri.parse(TestContract.TABLE1.CONTENT_URI + "/" + id);
+                break;
+            case 2:
+                throw  new UnsupportedOperationException("Insert not supported on URI: " + uri);
+            default:
+                throw new UnsupportedOperationException("Unknow Uri: " + uri);
+        }
+        return result;
     }
 
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        SQLiteDatabase database = testDatabaseHelper.getWritableDatabase();
+        switch (matcher.match(uri)){
+            case 1:
+                assert(database != null);
+                database.delete(TestContract.TABLE1.TABLE_NAME, selection, selectionArgs);
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
         return 0;
     }
 
