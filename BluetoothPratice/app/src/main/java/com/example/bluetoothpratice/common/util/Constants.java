482857e4f77e02a0d036cb0a58140ab3a00df0e7
zero <ail>
2020-08-11 19:02:44 +0800

    Google 官方demo

--- BluetoothPratice/app/src/main/java/com/example/bluetoothpratice/common/util/Constants.java
+++ BluetoothPratice/app/src/main/java/com/example/bluetoothpratice/common/util/Constants.java
@@ -1,5 +1,15 @@
 package com.example.bluetoothpratice.common.util;
 
 public class Constants {
-    public static final int REQUEST_BLUETOOTH_CODE = 1;
+
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
 }
