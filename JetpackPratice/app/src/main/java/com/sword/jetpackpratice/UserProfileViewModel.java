af2ed5691ca57310a94d1ebeca1a7438785209d5
zero <1163201968@qq.com>
2020-10-08 18:54:20 +0800

    null

---
+++ JetpackPratice/app/src/main/java/com/sword/jetpackpratice/UserProfileViewModel.java
@@ -0,0 +1,14 @@
+package com.sword.jetpackpratice;
+
+import android.widget.TextView;
+
+import androidx.lifecycle.ViewModel;
+
+public class UserProfileViewModel extends ViewModel {
+    User user;
+    String userId;
+
+//    public UserProfileViewModel(SavedStateHandle savedStateHandle) {
+//
+//    }
+}
