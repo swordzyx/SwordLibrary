53fab27804b9ddf730602d7fda2defcc71b2a520
zero <ail>
2020-11-03 16:06:22 +0800

    自定义 Layout

--- CustomView/CustomViewSet/app/src/main/java/com/sword/customviewset/layoutlayout/ColoredTextView.kt
+++ CustomView/CustomViewSet/app/src/main/java/com/sword/customviewset/layoutlayout/ColoredTextView.kt
@@ -22,7 +22,7 @@ private val COLORS = intArrayOf(
     Color.parseColor("#795548")
 )
 
-private val TEXT_SIZES = intArrayOf(16, 22, 28)
+private val TEXT_SIZES = floatArrayOf(16.dp, 22.dp, 28.dp)
 private val CORNER_RADIUS = 4.dp
 private val X_PADDING = 16.dp.toInt()
 private val Y_PADDING = 8.dp.toInt()
@@ -31,7 +31,6 @@ class ColoredTextView(context: Context, attrs: AttributeSet): AppCompatTextView(
     private val random = Random()
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        textSize = random.nextInt(TEXT_SIZES.size).toFloat()
         color = COLORS[random.nextInt(COLORS.size)]
     }
 
@@ -41,6 +40,8 @@ class ColoredTextView(context: Context, attrs: AttributeSet): AppCompatTextView(
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onDraw(canvas: Canvas) {
+        //在此处设置字体没有效果，而如果使用自带的 Paint 设置字体则会导致字体过大显示不全，目前没有想到解决办法，另外没并不像在花时间了，之后有时间有机会再来看把。
+        paint.textSize = TEXT_SIZES[random.nextInt(TEXT_SIZES.size)]
         canvas.drawRoundRect(0f, 0f, width.toFloat(), height.toFloat(), CORNER_RADIUS, CORNER_RADIUS, paint)
         super.onDraw(canvas)
     }
--- CustomView/CustomViewSet/app/src/main/java/com/sword/customviewset/layoutlayout/TagLayout.kt
+++ CustomView/CustomViewSet/app/src/main/java/com/sword/customviewset/layoutlayout/TagLayout.kt
@@ -1,79 +1,110 @@
 package com.sword.customviewset.layoutlayout
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.children
 import kotlin.math.max
 
 class TagLayout(context: Context, attrs: AttributeSet): ViewGroup(context, attrs) {
-    var usedWidth = 0
-    var usedHeight = 0
-    var childBoundsList = mutableListOf<Rect>()
+
+    private var childBoundsList = mutableListOf<Rect>()
 
     //widthMeasureSpec 和 heightMeasureSpec 是 Layout 的父 View 所提供的宽高值
+    @SuppressLint("DrawAllocation")
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val specWidthSize = MeasureSpec.getSize(widthMeasureSpec)
+        var usedWidth = 0
+        var usedHeight = 0
+        var lineUsedHeigh = 0
+        var lineUsedWidth = 0
         //获取 Layout 的期望，根据开发者所设定的宽高和父 View 限制的宽高条件
         for ((index, child) in children.withIndex()) {
             //测量子 View 的宽高，依赖于 Layout 的宽高，Layout 的宽高依赖于父 Layout 的期望宽高以及开发者所设置的宽高值
-            measureChild(child, widthMeasureSpec, heightMeasureSpec, usedWidth, usedHeight)
+//            measureChild(child, widthMeasureSpec, heightMeasureSpec, 0, usedHeight)
+            measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, usedHeight)
+
+            //保存所有子 View 的宽高，用于之后的布局。
+            if ((lineUsedWidth + child.measuredWidth) > specWidthSize) {
+                usedWidth = max(usedWidth, lineUsedWidth)
+                usedHeight += lineUsedHeigh
+                lineUsedHeigh = 0
+                lineUsedWidth = 0
+//                measureChild(child, widthMeasureSpec, heightMeasureSpec, 0, usedHeight)
+                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, usedHeight)
+            }
+
 
             if (index >= childBoundsList.size) {
                 childBoundsList.add(Rect())
             }
-            var rect = childBoundsList[index]
-            rect.set(usedWidth, usedHeight, usedWidth + child.measuredWidth, usedHeight + child.measuredHeight)
-            usedWidth += child.measuredWidth
-            usedHeight = max(usedHeight, child.measuredHeight)
+
+            val rect = childBoundsList[index]
+            rect.set(lineUsedWidth, usedHeight, lineUsedWidth + child.measuredWidth, usedHeight + child.measuredHeight)
+            lineUsedWidth += child.measuredWidth
+            lineUsedHeigh = max(lineUsedHeigh, child.measuredHeight)
         }
 
-
         //保存测量得到实际宽高
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(usedWidth, usedHeight + lineUsedHeigh)
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        for (child in children){
-            child.layout(0, 0, r - l, b - t)
+        for ((index,child) in children.withIndex()){
+            val bounds = childBoundsList[index]
+            child.layout(bounds.left, bounds.top, bounds.right, bounds.bottom)
         }
     }
 
-    fun measureChild(child: View, measureWidthSpec: Int, measureHeightSpec: Int, usedWidth: Int, usedHeight: Int) {
+    private fun measureChild(child: View, measureWidthSpec: Int, measureHeightSpec: Int, usedWidth: Int, usedHeight: Int) {
         //测量子 View 的宽高
-        var measureChildWidthSpec = computeSpec(layoutParams.width, measureWidthSpec, usedWidth)
-        var measureChildHeightSpec = computeSpec(layoutParams.height, measureHeightSpec, usedHeight)
+        Log.d("SWORD", "measure width")
+        val measureChildWidthSpec = computeSpec(layoutParams.width, measureWidthSpec, usedWidth)
+        Log.d("SWORD", "measure height")
+        val measureChildHeightSpec = computeSpec(layoutParams.height, measureHeightSpec, usedHeight)
         child.measure(measureChildWidthSpec, measureChildHeightSpec)
     }
 
-    fun computeSpec(layoutParams: Int, spec: Int, used: Int): Int{
+    private fun computeSpec(layoutParams: Int, spec: Int, used: Int): Int{
 
         var specChildMode = -1
         var specChildSize = -1
-        var measureSpecSize = MeasureSpec.getSize(spec)
-        var measureSpecMode = MeasureSpec.getMode(spec)
+        val measureSpecSize = MeasureSpec.getSize(spec)
+        val measureSpecMode = MeasureSpec.getMode(spec)
+        var modeString = ""
+        var layoutString = ""
 
         when(layoutParams) {
             LayoutParams.MATCH_PARENT -> {
+                layoutString = "MATCH_PARENT"
                 when(measureSpecMode){
+                    //在视频中，这里的 specChildMode 使用的是 EXACTLY，但是现在的代码中如果使用 EXACTLY，就会出现第一个子 View 就占满全屏的情况，原因是第一个子 view 的宽高就是全屏了。还没有找到解决方案。
                     MeasureSpec.AT_MOST, MeasureSpec.EXACTLY -> {
-                        specChildMode = MeasureSpec.EXACTLY
+                        modeString = "AT_MOST or EXACTLY"
+                        specChildMode = MeasureSpec.AT_MOST
                         specChildSize = measureSpecSize - used
                     }
                     MeasureSpec.UNSPECIFIED -> {
+                        modeString = "UNSPECIFIED"
                         specChildMode = MeasureSpec.UNSPECIFIED
                         specChildSize = 0
                     }
                 }
             }
             LayoutParams.WRAP_CONTENT -> {
+                layoutString = "WRAP_CONTENT"
                 when(MeasureSpec.getMode(spec)){
                     MeasureSpec.AT_MOST, MeasureSpec.EXACTLY -> {
+                        modeString = "AT_MOST or EXACTLY"
                         specChildMode = MeasureSpec.AT_MOST
                         specChildSize = measureSpecSize - used
                     }
                     MeasureSpec.UNSPECIFIED -> {
+                        modeString = "UNSPECIFIED"
                         specChildMode = MeasureSpec.UNSPECIFIED
                         specChildSize = 0
                     }
@@ -84,7 +115,12 @@ class TagLayout(context: Context, attrs: AttributeSet): ViewGroup(context, attrs
                 specChildSize = layoutParams
             }
         }
+        Log.d("SWORD", "layout param: $layoutString ; layout mode: $modeString; child size: $specChildSize")
         return MeasureSpec.makeMeasureSpec(specChildSize, specChildMode)
+    }
+
+    override fun generateLayoutParams(attrs: AttributeSet?): LayoutParams {
+        return MarginLayoutParams(context, attrs)
     }
 
 }
