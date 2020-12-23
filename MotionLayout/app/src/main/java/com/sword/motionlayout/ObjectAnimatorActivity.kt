7779a70e0e2e53dde1175b4d8d277cef327c3f3d
zero <1163201968@qq.com>
2020-12-23 23:53:16 +0800

    MotionLayout 临时提交

--- MotionLayout/app/src/main/java/com/sword/motionlayout/ObjectAnimatorActivity.kt
+++ MotionLayout/app/src/main/java/com/sword/motionlayout/ObjectAnimatorActivity.kt
@@ -1,14 +1,33 @@
 package com.sword.motionlayout
 
+import android.os.Build
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_object_animator.*
 import java.security.acl.Group
 
 class ObjectAnimatorActivity : AppCompatActivity(){
-    lateinit var root: Group
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_object_animator)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun onClick(v: View) {
+        TransitionManager.beginDelayedTransition(root)
+
+        with(v.layoutParams as FrameLayout.LayoutParams) {
+            gravity = Gravity.CENTER
+            height *= 2
+            width *=2
+        }
+
+        v.requestLayout()
     }
 }
