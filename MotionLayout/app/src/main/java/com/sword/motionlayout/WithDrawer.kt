e993df92f5e43ecc9aaa4f0877081e1295cde8c4
sword <1163201968@qq.com>
2021-01-06 09:19:21 +0800

    MotionLayout 示例代码

--- MotionLayout/app/src/main/java/com/sword/motionlayout/WithDrawer.kt
+++ MotionLayout/app/src/main/java/com/sword/motionlayout/WithDrawer.kt
@@ -3,9 +3,10 @@ package com.sword.motionlayout
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SampleActivity : AppCompatActivity() {
+class WithDrawer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample)
+        setContentView(R.layout.activity_drawer)
+
     }
 }
