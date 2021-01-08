3c07ea4b798d2d54f4ead3b0bdb3b9481c86fe3f
sword <1163201968@qq.com>
2021-01-08 09:31:54 +0800

    MotionLayoutActivity

--- MotionLayout/example/src/main/java/com/example/example/HenCoderActivity.kt
+++ MotionLayout/example/src/main/java/com/example/example/HenCoderActivity.kt
@@ -1,11 +1,18 @@
 package com.example.example
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.MotionLayout.DEBUG_SHOW_PATH
 
 class HenCoderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hen_coder)
+
+        findViewById<Button>(R.id.showDebug).setOnClickListener {
+            findViewById<MotionLayout>(R.id.motionLayout).setDebugMode(DEBUG_SHOW_PATH)
+        }
     }
 }
