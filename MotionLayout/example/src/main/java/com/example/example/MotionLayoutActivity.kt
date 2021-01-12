0ca67933cc98763ed0eb239859ec438334def4b8
sword <1163201968@qq.com>
2021-01-12 10:40:40 +0800

    MotionScene

--- MotionLayout/example/src/main/java/com/example/example/MotionLayoutActivity.kt
+++ MotionLayout/example/src/main/java/com/example/example/MotionLayoutActivity.kt
@@ -1,11 +1,17 @@
 package com.example.example
 
 import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MotionLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion_layout)
+
+        findViewById<RatingBar>(R.id.rating_film_rating).rating = 4.5f
+        findViewById<TextView>(R.id.text_film_title).text = getString(R.string.film_title)
+        findViewById<TextView>(R.id.text_film_description).text = getString(R.string.film_description);
     }
 }
