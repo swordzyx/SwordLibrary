177c1053489231e93cb908840e64da8aa6ecb350
sword <1163201968@qq.com>
2021-01-28 20:17:03 +0800

    倒计时器工具类 CountDownTimer

---
+++ SwordDemoLibrary/app/src/main/java/com/example/sworddemolibrary/CountDownTimerDemo.kt
@@ -0,0 +1,21 @@
+package com.example.sworddemolibrary
+
+import android.os.CountDownTimer
+import android.util.Log
+
+object CountDownTimerDemo {
+    private val TAG = "CountDownTimerDemo"
+    private val countDownTimer = object: CountDownTimer(3000L, 1000L) {
+        override fun onFinish() {
+            Log.d(TAG, "倒计时完成")
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+            Log.d(TAG, "倒计时：${millisUntilFinished}s");
+        }
+    }
+
+    fun start() {
+        countDownTimer.start()
+    }
+}
