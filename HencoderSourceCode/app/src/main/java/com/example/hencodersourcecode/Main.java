c53ceb2bf81864f5f0db801f082d680b075e7865
sword <1163201968@qq.com>
2021-03-13 15:19:14 +0800

    扔物线课堂代码练习

--- HencoderSourceCode/app/src/main/java/com/example/hencodersourcecode/Main.java
+++ HencoderSourceCode/app/src/main/java/com/example/hencodersourcecode/Main.java
@@ -1,8 +1,19 @@
 package com.example.hencodersourcecode;
 
+import java.sql.SQLOutput;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
     public static void main(String[] args) {
-        thread();
+//        thread();
+        threadFactory();
     }
 
     //使用 Thread 定义线程
@@ -28,5 +39,44 @@ public class Main {
         thread.start();
     }
 
+    //线程工厂
+    static void threadFactory() {
+        ThreadFactory factory = new ThreadFactory() {
+            int count = 0;
+            @Override
+            public Thread newThread(Runnable r) {
+                count ++;
+                return new Thread(r, count + " started");
+            }
+        };
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " Started !");
+            }
+        };
+
+        for (int i=0; i<1000; i++) {
+            factory.newThread(r).start();
+        }
+    }
+
+    static void executor() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread with Runnable started");
+            }
+        };
+
+        Executor executor = Executors.newCachedThreadPool();
+        executor.execute(runnable);
+        executor.execute(runnable);
+        executor.execute(runnable);
+
+        ExecutorService myExecutor = new ThreadPoolExecutor(5, 100, 5, TimeUnit.MINUTES, new SynchronousQueue<Runnable>());
+        myExecutor.execute(runnable);
+    }
 
 }
