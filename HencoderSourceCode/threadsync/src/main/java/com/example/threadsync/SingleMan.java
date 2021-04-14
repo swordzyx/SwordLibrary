07db9d8e699eedf58f958ea5a7cedecf633eb943
sword <1163201968@qq.com>
2021-04-14 10:06:55 +0800

    多线程与线程同步

---
+++ HencoderSourceCode/threadsync/src/main/java/com/example/threadsync/SingleMan.java
@@ -0,0 +1,21 @@
+package com.example.threadsync;
+
+public class SingleMan {
+    private static volatile SingleMan sInstance;
+
+    private SingleMan() {
+    }
+
+    static SingleMan newInstance() {
+        if (sInstance == null) {
+            synchronized (SingleMan.class) {
+                if (sInstance == null) {
+                    sInstance = new SingleMan();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+
+}
---
+++ HencoderSourceCode/threadsync/src/main/java/com/example/threadsync/TestDemo.java
@@ -0,0 +1,5 @@
+package com.example.threadsync;
+
+public interface TestDemo {
+    void runTest();
+}
