565a25dc236d051c4122e3453dc6bda0db49c69e
sword <1163201968@qq.com>
2021-05-01 18:01:48 +0800

    SingleJust, SingleMap, ObservableJust, ObservableInterval

--- HencoderSourceCode/ThreadRxjava/src/main/java/com/example/threadrxjava/Api.java
+++ HencoderSourceCode/ThreadRxjava/src/main/java/com/example/threadrxjava/Api.java
@@ -2,15 +2,15 @@ package com.example.threadrxjava;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.core.Single;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface Api {
     @GET("users/{username}/repos")
-    Call<List<Repo>> getRepos(@Path("username") String username);
+    Single<List<Repo>> getRepos(@Path("username") String username);
 
-
-    @GET("users/wanderingsword/repos")
-    Call<List<Repo>> getRepos1();
+    @GET("users/{username}/repos")
+    Call<List<Repo>> getCallRepos(@Path("username") String username);
 }
