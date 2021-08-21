2aa6d7e2cd26238fd1e8db0991a6173ef3b0a583
sword <1163201968@qq.com>
2021-08-21 09:50:15 +0800

    获取 Data Saver 首选项，请求后台数据

--- SwordDemoLibrary/connectivity/src/main/java/com/example/connectivity/util/NetworkUtilK.kt
+++ SwordDemoLibrary/connectivity/src/main/java/com/example/connectivity/util/NetworkUtilK.kt
@@ -1,10 +1,18 @@
 package com.example.connectivity.util
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.net.*
+import android.net.ConnectivityManager.*
 import android.os.Build
+import android.provider.Settings
 
 class NetworkUtilK {
+    /**
+     * 获取设备默认使用的网络的相关信息，并监听网络状态的更改
+     */
     fun readDefaultNetworkState(context: Context) {
         val connectivityManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             context.getSystemService(ConnectivityManager::class.java)
@@ -14,7 +22,8 @@ class NetworkUtilK {
 
         //Android 7.0
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+            connectivityManager.registerDefaultNetworkCallback(object :
+                ConnectivityManager.NetworkCallback() {
                 //有一个新网络可用时，回调该方法。对于之前传递到此方法中的 Network，将不会触发此回调
                 //如果使用 registerDefaultNetworkCallback 注册的回调，那么仅当有一个新的最佳网络可用时，会触发该回调
                 override fun onAvailable(network: Network) {
@@ -38,22 +47,32 @@ class NetworkUtilK {
                 }
 
                 //网络的 capability（功能） 发生变更时调用
-                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
+                override fun onCapabilitiesChanged(
+                    network: Network,
+                    networkCapabilities: NetworkCapabilities
+                ) {
                     LogUtil.INSTANCE.debug("onCapabilitiesChanged")
                 }
 
                 //网络的 LinkProperty 发生变更时回调
-                override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
+                override fun onLinkPropertiesChanged(
+                    network: Network,
+                    linkProperties: LinkProperties
+                ) {
                     LogUtil.INSTANCE.debug("onLinkPropertiesChanged")
                 }
 
                 //网络的阻塞状态发生变更时回调（网络变为阻塞或者变为非阻塞）
                 override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
-                    LogUtil.INSTANCE.debug("onBlockedStatusChanged")                }
+                    LogUtil.INSTANCE.debug("onBlockedStatusChanged")
+                }
             })
         }
     }
 
+    /**
+     * 获取设备连接的所有网络的相关信息，并监听网络状态的更改
+     */
     fun readAllNetworkState(context: Context) {
         val connectivityManager = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
             context.getSystemService(ConnectivityManager::class.java)
@@ -69,28 +88,94 @@ class NetworkUtilK {
                 .build()
 
             //监听所有未计量网络的状态的更改，NetworkRequest 用于配置网络的过滤规则，可以监听指定 Capability 和 Transport 的网络的状态的变更
-            connectivityManager.registerNetworkCallback(networkRequest, object : ConnectivityManager.NetworkCallback() {
-                override fun onAvailable(network: Network) {
-                }
+            connectivityManager.registerNetworkCallback(
+                networkRequest,
+                object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                    }
 
-                override fun onLosing(network: Network, maxMsToLive: Int) {
-                }
+                    override fun onLosing(network: Network, maxMsToLive: Int) {
+                    }
 
-                override fun onLost(network: Network) {
-                }
+                    override fun onLost(network: Network) {
+                    }
 
-                override fun onUnavailable() {
-                }
+                    override fun onUnavailable() {
+                    }
 
-                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
-                }
+                    override fun onCapabilitiesChanged(
+                        network: Network,
+                        networkCapabilities: NetworkCapabilities
+                    ) {
+                    }
 
-                override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
-                }
+                    override fun onLinkPropertiesChanged(
+                        network: Network,
+                        linkProperties: LinkProperties
+                    ) {
+                    }
 
-                override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
+                    override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
+                    }
+                })
+        }
+    }
+
+    /**
+     * 获取数据使用限制
+     */
+    fun getDataUsageLimit(context: Context) {
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).apply {
+            //设备使用的是计量网络
+            if (isActiveNetworkMetered) {
+                // restrictBackgroundStatus 是 Android 7.0 扩展的成员
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    when (restrictBackgroundStatus) {
+                        //用户启用 Data Saver，应用需要限制前台数据的使用以及处理后台数据限制
+                        RESTRICT_BACKGROUND_STATUS_ENABLED -> {
+
+                        }
+                        //用户允许应用绕过 Data Saver
+                        RESTRICT_BACKGROUND_STATUS_WHITELISTED -> {
+
+                        }
+                        //用户禁用 Data Saver
+                        RESTRICT_BACKGROUND_STATUS_DISABLED -> {
+
+                        }
+                    }
                 }
-            })
+            } else {
+                //设备使用的是非计量网络
+            }
+        }
+    }
+
+    /**
+     * 跳转至 Settings 应用，请求使用后台数据
+     */
+    fun requestDataRestriction(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            //在发送 Intent 之前，可以询问用户是否想要跳转至 Settings 应用
+
+            val intent = Intent(Settings.ACTION_IGNORE_BACKGROUND_DATA_RESTRICTIONS_SETTINGS)
+            intent.data = Uri.parse("package:" + context.packageName)
+            context.startActivity(intent)
+        }
+    }
+
+    /**
+     * 监听 Data Saver 首选项的更改
+     */
+    fun monitorDataSaverChange(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            context.registerReceiver(broadcastReceiver, IntentFilter(ACTION_RESTRICT_BACKGROUND_CHANGED))
+        }
+    }
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            //Data Saver 首选项发生变更
         }
     }
 }
