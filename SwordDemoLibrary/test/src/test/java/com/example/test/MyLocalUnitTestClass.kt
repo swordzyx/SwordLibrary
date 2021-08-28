f30d5c4fe139a2bef7c25de28fec01625841514b
sword <1163201968@qq.com>
2021-08-28 23:06:44 +0800

    Mockito test pratice code

--- SwordDemoLibrary/test/src/test/java/com/example/test/MyLocalUnitTestClass.kt
+++ SwordDemoLibrary/test/src/test/java/com/example/test/MyLocalUnitTestClass.kt
@@ -2,12 +2,11 @@ package com.example.test
 
 import android.content.Context
 import com.google.common.truth.Truth.assertThat
-import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.BDDMockito.given
 import org.mockito.Mock
-import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 
 private const val FAKE_STRING = "HELLO_WORLD"
@@ -15,25 +14,17 @@ private const val FAKE_STRING = "HELLO_WORLD"
 @RunWith(MockitoJUnitRunner::class)
 class MyLocalUnitTestClass {
 
-
     @Mock
     private lateinit var context: Context
 
-    lateinit var closeable: AutoCloseable
-
     @Before
-    fun openMocks() {
-        closeable = MockitoAnnotations.openMocks(this)
+    fun initMocks() {
+        given(context.getString(R.string.hello_world)).willReturn(FAKE_STRING)
     }
 
     @Test
     fun readStringFromContext_LocalizedString() {
         val classUnderTest = ClassUnterTest(context)
         assertThat(classUnderTest.getHelloWorldString()).isEqualTo(FAKE_STRING)
-    }
-
-    @After
-    fun releaseMocks() {
-        closeable.close()
     }
 }
