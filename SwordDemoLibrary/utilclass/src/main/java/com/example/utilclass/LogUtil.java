e6a18aa0079915e08b37c309ee33b9a12c23a2c4
sword <1163201968@qq.com>
2021-09-02 18:50:18 +0800

    增加日志工具类

--- SwordDemoLibrary/utilclass/src/main/java/com/example/utilclass/LogUtil.java
+++ SwordDemoLibrary/utilclass/src/main/java/com/example/utilclass/LogUtil.java
@@ -1,2 +1,18 @@
-package com.example.utilclass;public class LogUtil {
+package com.example.utilclass;
+
+import android.util.Log;
+
+public class LogUtil {
+    private static final String TAG = "Sword"; 
+    public static void debug(String msg) {
+        Log.d(TAG, msg);
+    }
+    
+    public static void error(String msg) {
+        Log.d(TAG, msg);
+    }
+    
+    public static void warn(String msg) {
+        Log.d(TAG, msg);
+    }
 }
