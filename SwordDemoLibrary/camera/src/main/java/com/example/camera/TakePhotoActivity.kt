66c28c70f4e9a8a53b82c876028bb0e7ef8c894e
sword <1163201968@qq.com>
2021-09-15 19:31:34 +0800

    android Developer Sample

--- SwordDemoLibrary/camera/src/main/java/com/example/camera/TakePhotoActivity.kt
+++ SwordDemoLibrary/camera/src/main/java/com/example/camera/TakePhotoActivity.kt
@@ -2,19 +2,23 @@ package com.example.camera
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.take_photo_activity.*
 
 class TakePhotoActivity : AppCompatActivity() {
     private val REQUEST_IMAGE_CAPTURE = 1
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+        setContentView(R.layout.take_photo_activity)
+
     }
-    
+
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         try {
@@ -27,8 +31,15 @@ class TakePhotoActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == REQUEST_IMAGE_CAPTURE) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
+    }
 
+    fun takePhoto(view: View) {
+        if (view.id == R.id.take_photo) {
+            dispatchTakePictureIntent()
         }
     }
 }
