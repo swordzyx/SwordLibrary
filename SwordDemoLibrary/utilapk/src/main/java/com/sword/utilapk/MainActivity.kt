a40636380c1c18aee21afa781c8784a68838ed3f
sword <1163201968@qq.com>
2021-11-18 13:41:45 +0800

    读取外部存储状态，通过文件是否存在控制是否开启日志打印，Retrofit sample

--- SwordDemoLibrary/utilapk/src/main/java/com/sword/utilapk/MainActivity.kt
+++ SwordDemoLibrary/utilapk/src/main/java/com/sword/utilapk/MainActivity.kt
@@ -9,27 +9,27 @@ import java.io.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-        if (!isPermissionGranted(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            requestSinglePermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        }
+    if (!isPermissionGranted(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+      requestSinglePermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
     }
+  }
 
-    fun onClick(view: android.view.View) {
-        
-    }
+  fun onClick(view: android.view.View) {
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-            finish()
-        }
+  }
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+      finish()
     }
+  }
 }
--- SwordDemoLibrary/utilclass/src/main/java/com/example/utilclass/LogUtil.java
+++ SwordDemoLibrary/utilclass/src/main/java/com/example/utilclass/LogUtil.java
@@ -6,26 +6,26 @@ import android.util.Log;
 import java.io.File;
 
 public class LogUtil {
-    private static final String TAG = "Sword";
-    private static final String LOG_SWITCH_DIR = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "LoggerSwitch";
-    private static final File LOG_SWITCH_FILE  = new File(LOG_SWITCH_DIR 
-            + File.separator 
-            + Encryption.md5(LOG_SWITCH_DIR + File.separator + "logger_switch"));
+  private static final String TAG = "Sword";
+  private static final String LOG_SWITCH_DIR = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "LoggerSwitch";
+  private static final File LOG_SWITCH_FILE = new File(LOG_SWITCH_DIR
+      + File.separator
+      + Encryption.md5(LOG_SWITCH_DIR + File.separator + "logger_switch"));
+
+
+  private static boolean isDebug() {
+    return LOG_SWITCH_FILE.exists();
+  }
+
+  public static void debug(String msg) {
+    Log.d(TAG, msg);
+  }
     
-    
-    private static boolean isDebug() {
-        return LOG_SWITCH_FILE.exists();
-    }
-    
-    public static void debug(String msg) {
-        Log.d(TAG, msg);
-    }
-    
-    public static void error(String msg) {
-        Log.d(TAG, msg);
-    }
-    
-    public static void warn(String msg) {
-        Log.d(TAG, msg);
-    }
+  public static void error(String msg) {
+    Log.d(TAG, msg);
+  }
+
+  public static void warn(String msg) {
+    Log.d(TAG, msg);
+  }
 }
