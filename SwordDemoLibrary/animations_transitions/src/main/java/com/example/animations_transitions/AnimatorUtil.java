c74cb0aaf433188bbbff5154f42349c4204c3c53
sword <1163201968@qq.com>
2021-11-22 12:59:39 +0800

    属性动画

---
+++ SwordDemoLibrary/animations_transitions/src/main/java/com/example/animations_transitions/AnimatorUtil.java
@@ -0,0 +1,45 @@
+package com.example.animations_transitions;
+
+import android.animation.AnimatorSet;
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.utilclass.ScreenSizeUtil;
+
+public class AnimatorUtil {
+  private final ScreenSizeUtil sizeUtil = new ScreenSizeUtil();
+  
+  public void startAnimate(ImageView target) {
+    ObjectAnimator animator = ObjectAnimator.ofFloat(target, "alpha", sizeUtil.dpToPx(200));
+    animator.start();
+  }
+  
+  public void propertyValuesHolder(CircleView target) {
+    PropertyValuesHolder holder1 = PropertyValuesHolder.ofFloat("radius", sizeUtil.dpToPx(200));
+    PropertyValuesHolder holder2 = PropertyValuesHolder.ofFloat("offset", sizeUtil.dpToPx(100));
+    ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(target, holder1, holder2);
+    animator.start();
+  }
+  
+  public void propertyValuesHolderWithKeyframe(CircleView target) {
+    Keyframe keyframe1 = Keyframe.ofFloat(0, sizeUtil.dpToPx(100));
+    Keyframe keyframe2 = Keyframe.ofFloat(0.5f, sizeUtil.dpToPx(250));
+    Keyframe keyframe3 = Keyframe.ofFloat(1, sizeUtil.dpToPx(200));
+    
+    PropertyValuesHolder holder = PropertyValuesHolder.ofKeyframe("radius", keyframe1, keyframe2, keyframe3);
+    ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(target, holder);
+    animator.start();
+  }
+  
+  public void animatorSet(CircleView target) {
+    ObjectAnimator animator1 = ObjectAnimator.ofFloat(target, "radius", sizeUtil.dpToPx(200));
+    ObjectAnimator animator2 = ObjectAnimator.ofFloat(target, "offset", sizeUtil.dpToPx(100));
+    
+    AnimatorSet animatorSet = new AnimatorSet();
+    animatorSet.playTogether(animator1, animator2);
+    animatorSet.start();
+  }
+}
--- SwordDemoLibrary/floatball/src/main/java/com/sword/floatball/FloatMenuView.java
+++ SwordDemoLibrary/floatball/src/main/java/com/sword/floatball/FloatMenuView.java
@@ -1,4 +1,16 @@
 package com.sword.floatball;
 
-public class FloatMenuView {
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class FloatMenuView extends ViewGroup {
+  public FloatMenuView(Context context) {
+    super(context);
+  }
+
+  @Override
+  protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+    
+  }
 }
