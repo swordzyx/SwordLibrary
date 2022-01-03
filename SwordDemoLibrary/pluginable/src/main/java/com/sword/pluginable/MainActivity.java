994a75ad112430a8395d87042121c09f9cb70dde
sword <1163201968@qq.com>
2022-01-03 13:52:48 +0800

    插件化，热更新

--- SwordDemoLibrary/pluginable/src/main/java/com/sword/pluginable/MainActivity.java
+++ SwordDemoLibrary/pluginable/src/main/java/com/sword/pluginable/MainActivity.java
@@ -1,14 +1,20 @@
 package com.sword.pluginable;
 
 import android.os.Bundle;
-import android.os.FileUtils;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+import dalvik.system.DexClassLoader;
+import okio.BufferedSink;
+import okio.Okio;
+import okio.Source;
 
 public class MainActivity extends AppCompatActivity {
 	@Override
@@ -16,30 +22,20 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-
-	}
-
-	public static void main(String[] args) {
-		//reflectInvoke();
-		reflectInvokePrivate();
+		//reflectInvokePrivate();
+		refectInvokeOtherApk();
 	}
 
 	/**
 	 * 反射访问 public 修饰的类
 	 */
-	private static void reflectInvoke() {
-		Class cls = PublicUtils.class;
+	private void reflectInvoke() {
+		Class<?> cls = PublicUtils.class;
 		try {
 			Object publicUtils = cls.newInstance();
 			Method shoutMethod = cls.getDeclaredMethod("shout");
 			shoutMethod.invoke(publicUtils);
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
+		} catch (IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
 			e.printStackTrace();
 		}
 
@@ -49,10 +45,10 @@ public class MainActivity extends AppCompatActivity {
 	/**
 	 * 反射访问非公开的类，通过 class.forName("${className}")
 	 */
-	private static void reflectInvokePrivate() {
+	private void reflectInvokePrivate() {
 		try {
-			Class cls = Class.forName("com.sword.pluginable.PrivateUtils");
-			Constructor constructor = cls.getDeclaredConstructors()[0];
+			Class<?> cls = Class.forName("com.sword.pluginable.PrivateUtils");
+			Constructor<?> constructor = cls.getDeclaredConstructors()[0];
 			constructor.setAccessible(true);
 			Object PrivateUtils = constructor.newInstance();
 			Method shoutMethod = cls.getDeclaredMethod("shout");
@@ -66,9 +62,25 @@ public class MainActivity extends AppCompatActivity {
 	/**
 	 * 访问同一个工程下其他 apk 模块的类，使用 ClassLoader
 	 */
-	private static void refectInvokeOtherApk() {
+	private void refectInvokeOtherApk() {
 		//先将 assets/plugin-debug.apk 拷贝到缓存目录中
+		File apk = new File(getCacheDir() + "/plugin.apk");
+		try(Source source = Okio.source(getAssets().open("plugin-debug.apk"));
+				BufferedSink sink = Okio.buffer(Okio.sink(apk))) {
+			sink.writeAll(source);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
+		DexClassLoader classLoader = new DexClassLoader(apk.getPath(), getCacheDir().getPath(), null, null);
+		try {
+			Class<?> cls = classLoader.loadClass("com.example.plugin.PublicUtils");
+			Object publicUtils = cls.newInstance();
+			Method shoutMethod = cls.getDeclaredMethod("shout");
+			shoutMethod.invoke(publicUtils);
+		} catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
