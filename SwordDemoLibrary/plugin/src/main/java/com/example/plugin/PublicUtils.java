7cbfe9af724f1635e7054ac2252b3c5b40774f0a
zero <1163201968@qq.com>
2022-01-04 09:41:59 +0800

    手写热更心

--- SwordDemoLibrary/plugin/src/main/java/com/example/plugin/PublicUtils.java
+++ SwordDemoLibrary/plugin/src/main/java/com/example/plugin/PublicUtils.java
@@ -1,8 +1,12 @@
 package com.example.plugin;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class PublicUtils {
 
-	public void shout() {
+	public void shout(Context context) {
 		System.out.println("I'm shouting from a plugin!");
+		Toast.makeText(context, "I'm shouting from a plugin!", Toast.LENGTH_LONG).show();
 	}
 }
--- SwordDemoLibrary/pluginable/src/main/java/com/sword/pluginable/MainActivity.java
+++ SwordDemoLibrary/pluginable/src/main/java/com/sword/pluginable/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sword.pluginable;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		//reflectInvokePrivate();
-		refectInvokeOtherApk();
+		//refectInvokeOtherApk();
 	}
 
 	/**
@@ -47,12 +48,16 @@ public class MainActivity extends AppCompatActivity {
 	 */
 	private void reflectInvokePrivate() {
 		try {
+			//执行 PrivateUtils#shout()
 			Class<?> cls = Class.forName("com.sword.pluginable.PrivateUtils");
+			//获取构造方法，并通过构造方法获取实例
 			Constructor<?> constructor = cls.getDeclaredConstructors()[0];
 			constructor.setAccessible(true);
 			Object PrivateUtils = constructor.newInstance();
+			//获取 PrivateUtils#shout 方法实例
 			Method shoutMethod = cls.getDeclaredMethod("shout");
 			shoutMethod.setAccessible(true);
+			//执行 PrivateUtils.shout() 方法
 			shoutMethod.invoke(PrivateUtils);
 		} catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
 			e.printStackTrace();
@@ -65,22 +70,31 @@ public class MainActivity extends AppCompatActivity {
 	private void refectInvokeOtherApk() {
 		//先将 assets/plugin-debug.apk 拷贝到缓存目录中
 		File apk = new File(getCacheDir() + "/plugin.apk");
-		try(Source source = Okio.source(getAssets().open("plugin-debug.apk"));
-				BufferedSink sink = Okio.buffer(Okio.sink(apk))) {
-			sink.writeAll(source);
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (!apk.exists()) {
+			try(Source source = Okio.source(getAssets().open("plugin-debug.apk"));
+			    BufferedSink sink = Okio.buffer(Okio.sink(apk))) {
+				sink.writeAll(source);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if (apk.exists()) {
+			DexClassLoader classLoader = new DexClassLoader(apk.getPath(), getCacheDir().getPath(), null, null);
+			try {
+				Class<?> cls = classLoader.loadClass("com.example.plugin.PublicUtils");
+				Object publicUtils = cls.newInstance();
+				Method shoutMethod = cls.getDeclaredMethod("shout");
+				shoutMethod.invoke(publicUtils);
+			} catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
+				e.printStackTrace();
+			}
 		}
 
-		DexClassLoader classLoader = new DexClassLoader(apk.getPath(), getCacheDir().getPath(), null, null);
-		try {
-			Class<?> cls = classLoader.loadClass("com.example.plugin.PublicUtils");
-			Object publicUtils = cls.newInstance();
-			Method shoutMethod = cls.getDeclaredMethod("shout");
-			shoutMethod.invoke(publicUtils);
-		} catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
-			e.printStackTrace();
-		}
+	}
 
+	public void onClick(View view) {
+		if (view.getId() == R.id.loadPlugin) {
+			refectInvokeOtherApk();
+		}
 	}
 }
