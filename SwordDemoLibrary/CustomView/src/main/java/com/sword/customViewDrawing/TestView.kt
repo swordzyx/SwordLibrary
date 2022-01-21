7fad1fa87199b0aaaecf1599ac997050f28308a1
zero <1163201968@qq.com>
2022-01-21 09:15:40 +0800

    扔物线 - 自定义 View

---
+++ SwordDemoLibrary/CustomView/src/main/java/com/sword/customViewDrawing/TestView.kt
@@ -0,0 +1,5 @@
+package com.sword.customViewDrawing
+
+class TestView {
+
+}
---
+++ SwordDemoLibrary/CustomView/src/main/java/com/sword/customViewDrawing/Utils.java
@@ -0,0 +1,9 @@
+package com.sword.customViewDrawing;
+
+import android.util.TypedValue;
+
+public class Utils {
+	public static float dp2px(float value) {
+		return TypedValue.applyDimension()
+	}
+}
