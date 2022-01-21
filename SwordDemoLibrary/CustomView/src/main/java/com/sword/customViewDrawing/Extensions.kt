e860812c30e8c5aa7fff16aa99d9115dcd46db59
zero <1163201968@qq.com>
2022-01-21 23:55:44 +0800

    自定义 View，Path & PathMeasure 基本用法

--- SwordDemoLibrary/CustomView/src/main/java/com/sword/customViewDrawing/Extensions.kt
+++ SwordDemoLibrary/CustomView/src/main/java/com/sword/customViewDrawing/Extensions.kt
@@ -3,5 +3,6 @@ package com.sword.customViewDrawing
 import android.content.res.Resources
 import android.util.TypedValue
 
-//Resources.getSystem() 只能拿到系统相关的资源，拿不到应用内部的资源。屏幕密度及尺寸相关的信息是放在系统的资源里面的。
-fun Float.dp() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics) 
+//Resources.getSystem() 只能拿到系统相关的资源，拿不到应用内部的资源。屏幕密度及尺寸相关的信息是放在系统的资源里面的。单位的转换是跟应用的资源无关的，所有的应用使用的屏幕密度是一样的。
+val Float.dp
+		get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics)
