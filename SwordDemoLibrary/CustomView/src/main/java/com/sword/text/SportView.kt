5b6ba464e2c849a37103d7046904bae3fb9d7576
zero <1163201968@qq.com>
2022-03-02 00:14:23 +0800

    sync

---
+++ SwordDemoLibrary/CustomView/src/main/java/com/sword/text/SportView.kt
@@ -0,0 +1,12 @@
+package com.sword.text
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class SportView(context: Context, attrs: AttributeSet): View(context, attrs) {
+	override fun onDraw(canvas: Canvas) {
+		//drawText：绘制文字
+	}
+}
--- SwordDemoLibrary/CustomView/src/main/java/com/sword/xfermode/XfermodeView.kt
+++ SwordDemoLibrary/CustomView/src/main/java/com/sword/xfermode/XfermodeView.kt
@@ -9,10 +9,26 @@ import com.sword.customViewDrawing.dp
 
 class XfermodeView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 	private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+	private val bounds = RectF(150f.dp, 50f.dp, 300f.dp, 200f.dp)
 	private val circleBitmap = Bitmap.createBitmap(150f.dp.toInt(), 150f.dp.toInt(), Bitmap.Config.ARGB_8888)
 	private val squareBitmap = Bitmap.createBitmap(150f.dp.toInt(), 150f.dp.toInt(), Bitmap.Config.ARGB_8888)
+	private val xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
 
 	init {
-		circleBitmap
+		val canvas = Canvas(circleBitmap)
+		paint.color = Color.parseColor("#D81B60")
+		canvas.drawOval(50f.dp, 0f.dp, 150f.dp, 100f.dp, paint)
+		paint.color = Color.parseColor("#2196F3")
+		canvas.setBitmap(squareBitmap)
+		canvas.drawRect(0f.dp, 50f.dp, 100f.dp, 150f.dp, paint)
+	}
+
+	override fun onDraw(canvas: Canvas) {
+		val count = canvas.saveLayer(bounds, null)
+		canvas.drawBitmap(circleBitmap, 150f.dp, 50f.dp, paint)
+		paint.xfermode = xfermode
+		canvas.drawBitmap(squareBitmap, 150f.dp, 50f.dp, paint)
+		paint.xfermode = null
+		canvas.restoreToCount(count)
 	}
 }
