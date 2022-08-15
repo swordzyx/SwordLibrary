1803ff1c1dcb944e83b9f8d89d44351dbf14bc5f
sword <1163201968@qq.com>
2022-08-15 16:58:14 +0800

    增加 openprojectcode 模块，用于保存学习时所写的 Sample 代码

---
+++ SwordDemoLibrary/openprojectcode/src/main/java/com/sword/openprojectcode/AntiAospUtils.java
@@ -0,0 +1,14 @@
+package com.sword.openprojectcode;
+
+/**
+ * 参考：https://www.jianshu.com/p/e3a9c4f752c5
+ */
+public class AntiAospUtils {
+
+  /**
+   * 扫描 CPU 架构信息
+   */
+  private void checkDeviceForumX86() {
+    
+  }
+}
