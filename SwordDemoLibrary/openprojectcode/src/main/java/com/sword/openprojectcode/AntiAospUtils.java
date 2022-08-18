a3a9f6b7af243561978f12c7bf3e5823cf9739df
sword <1163201968@qq.com>
2022-08-18 18:47:13 +0800

    https://www.jianshu.com/p/e3a9c4f752c5 文章学习

--- SwordDemoLibrary/openprojectcode/src/main/java/com/sword/openprojectcode/AntiAospUtils.java
+++ SwordDemoLibrary/openprojectcode/src/main/java/com/sword/openprojectcode/AntiAospUtils.java
@@ -1,9 +1,18 @@
 package com.sword.openprojectcode;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import com.sword.LogUtil;
+
 /**
  * 参考：https://www.jianshu.com/p/e3a9c4f752c5
  */
 public class AntiAospUtils {
+  public static final String tag = "AntiAospUtils";
+  
+  private static final String NATIVE_LIB_X86 = "x86";
 
   /**
    * 扫描 CPU 架构信息
@@ -11,4 +20,18 @@ public class AntiAospUtils {
   private void checkDeviceForumX86() {
     
   }
+  
+  private String getNativeLibDirName(Context context, String packageName) {
+    try {
+      PackageInfo packageInfo = context.getPackageManager().getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+      String nativeLibraryDir = packageInfo.applicationInfo.nativeLibraryDir;
+      LogUtil.debug(tag, "getNativeLibDirName, packageName: " + packageName + ", nativeLibraryDirName: " + nativeLibraryDir);
+      return nativeLibraryDir;
+    } catch (PackageManager.NameNotFoundException exception) {
+      exception.printStackTrace();
+    }
+    return "";
+  }
+  
+  
 }
