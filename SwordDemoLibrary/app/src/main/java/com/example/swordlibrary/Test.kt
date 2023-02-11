e023a03a4043c88514b15ba8daa9db5cdbff1d8f
wanderingsword <35715272+wanderingsword@users.noreply.github.com>
2023-02-11 19:28:48 +0800

    flatMapLatest SampleCode，flow Sample Code

--- SwordDemoLibrary/app/src/main/java/com/example/swordlibrary/Test.kt
+++ SwordDemoLibrary/app/src/main/java/com/example/swordlibrary/Test.kt
@@ -1,28 +1,9 @@
 package com.example.swordlibrary
 
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.flatMapLatest
+import com.example.swordlibrary.kotlin.flatMapLatestTest
+import kotlinx.coroutines.runBlocking
 
-@OptIn(ExperimentalCoroutinesApi::class)
 fun main() = runBlocking {
-  flow {
-    emit("a")
-    delay(100)
-    emit("b")
-  }.flatMapLatest { value ->
-    flow {
-      emit(value)
-      delay(200)
-      emit(value + "_last")
-    }
-  }.collect { value ->
-    println("value: $value")
-  }
-  /**
-   * 输出结果：当源 flow 生产出 b 之后，为 a 创建的 flow 就被取消掉了，所以 a_last 还没来得及打印。
-   * value: a
-   * value: b
-   * value: b_last
-   */
+  flatMapLatestTest()
 }
+
