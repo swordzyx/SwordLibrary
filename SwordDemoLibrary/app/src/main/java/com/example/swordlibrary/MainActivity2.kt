085af22a6a0b053bcee770cce93104583ea6ac03
wanderingsword <1163201968@qq.com>
2023-03-11 17:52:42 +0800

    扔物线高级课程作业 - 自定义布局尺寸

--- SwordDemoLibrary/app/src/main/java/com/example/swordlibrary/MainActivity2.kt
+++ SwordDemoLibrary/app/src/main/java/com/example/swordlibrary/MainActivity2.kt
@@ -15,7 +15,7 @@ class MainActivity2: AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    setContentView(R.layout.activity_main2)
+    setContentView(R.layout.activity_main3)
     initWindowSize(this)
 
 
