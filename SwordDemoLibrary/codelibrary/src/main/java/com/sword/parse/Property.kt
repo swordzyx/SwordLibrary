8afa0c34c0b7d5c15df829844cddf26a60525f6f
wanderingsword <1163201968@qq.com>
2023-06-06 13:43:30 +0800

    移除无用代码

--- SwordDemoLibrary/codelibrary/src/main/java/com/sword/parse/Property.kt
+++
@@ -1,4 +0,0 @@
-package com.example.utilclass.apk.parse
-
-class Property(val key: String, val value: String) {
-}
--- SwordDemoLibrary/codelibrary/src/main/java/com/sword/parser/AlertUtil.kt
+++
@@ -1,5 +0,0 @@
-package com.example.utilclass.net.dongliu.apk.parser
-
-fun showAlert(content: String) {
-  println(content)
-}
