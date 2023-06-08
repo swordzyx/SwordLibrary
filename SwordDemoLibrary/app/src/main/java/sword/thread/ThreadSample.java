1e90aef787896b4ca504e8c556e7e970a0a9ca77
wanderingsword <1163201968@qq.com>
2023-06-08 13:48:31 +0800

    移除无用代码，扔物线 - 多线程和线程同步 Callable 示例

--- SwordDemoLibrary/app/src/main/java/sword/thread/ThreadSample.java
+++ SwordDemoLibrary/app/src/main/java/sword/thread/ThreadSample.java
@@ -1,7 +1,10 @@
 package sword.thread;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,7 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ThreadSample {
 
   public static void main(String[] args) {
-    threadFactory();
+    callable();
   }
   public static void threadFactory() {
     ThreadFactory threadFactory = new ThreadFactory() {
@@ -28,7 +31,29 @@ public class ThreadSample {
   }
   
   public static void callable() {
+    //创建一个 Callable
+    Callable<String> callable = () -> {
+      try {
+        Thread.sleep(1500);
+      } catch (InterruptedException ignored){}
+      return "Callable Done~~~";
+    };
+    //执行这个 Callable
     ExecutorService executorService = Executors.newCachedThreadPool();
-    executorService.submit()
+    Future<String> callableFuture = executorService.submit(callable);
+
+    //获取并打印 Callable 的执行结果
+    while(true) {
+      if (callableFuture.isDone()) {
+        try {
+          System.out.println(callableFuture.get());
+        } catch (ExecutionException | InterruptedException e) {
+          e.printStackTrace();
+        }
+        break;
+      }
+    }
+
+
   }
 }
