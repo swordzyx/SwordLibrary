4a80007c1f98662cb9feda77a453de8eded3a1bf
wanderingsword <1163201968@qq.com>
2023-09-18 13:52:05 +0800

    扔物线高级课程 - BlockCanary 源码解析 - 自定义 Gradle 插件修改 class 字节码

--- SwordDemoLibrary/buildSrc/src/main/kotlin/com/sword/plugin/TracePlugin.kt
+++ SwordDemoLibrary/buildSrc/src/main/kotlin/com/sword/plugin/TracePlugin.kt
@@ -5,11 +5,9 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class TracePlugin: Plugin<Project> {
-  
   override fun apply(project: Project) {
     project.extensions
       .getByType(AppExtension::class.java)
       .registerTransform(TraceTransform())
-    
   }
 }
