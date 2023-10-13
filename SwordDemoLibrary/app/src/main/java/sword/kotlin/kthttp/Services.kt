fda0fe7eac0a5fb18892a50f29deebc75855f353
sword <35715272+wanderingsword@users.noreply.github.com>
2023-10-13 16:38:57 +0800

    kotlin编程第一课 - 网络请求框架支持挂起函数

--- SwordDemoLibrary/app/src/main/java/sword/kotlin/kthttp/Services.kt
+++ SwordDemoLibrary/app/src/main/java/sword/kotlin/kthttp/Services.kt
@@ -7,4 +7,10 @@ interface GithubService {
     @Field("lang") lang: String,
     @Field("since") since: String
   ): RepoList
+
+  @GET("/repo")
+  fun reposV2(
+    @Field("lang") lang: String, 
+    @Field("since") since: String
+  ): KtCall<RepoList>
 }
