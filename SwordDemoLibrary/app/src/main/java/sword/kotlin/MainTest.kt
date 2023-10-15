3f7b8cd29c64d6094c9b4c52ff72a3d7156e47a2
wanderingsword <1163201968@qq.com>
2023-10-15 19:52:37 +0800

    kotlin 编程第一课 - Flow

--- SwordDemoLibrary/app/src/main/java/sword/kotlin/MainTest.kt
+++ SwordDemoLibrary/app/src/main/java/sword/kotlin/MainTest.kt
@@ -4,12 +4,23 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
@@ -18,22 +29,75 @@ import kotlin.random.Random
 import kotlin.system.measureTimeMillis
 
 
+
+
+fun main() {
+  flowSample()
+}
+
+
+fun flowSample() = runBlocking {
+  flow {
+    emit(1)
+    emit(2)
+    emit(3)
+    emit(4)
+    emit(5)
+  }.filter { it > 2 }
+    .map {
+      it * 2
+    }
+    .take(2)
+    .collect {
+      logx("collect: $it")
+    }
+
+  flowOf(1, 2, 3, 4, 5).filter { it > 2 }
+    .map {
+      it * 2
+    }
+    .take(2)
+    .collect {
+      logx("collect: $it")
+    }
+}
+
+fun channelSample() = runBlocking {
+  val channel = Channel<Int>(
+    capacity = Channel.CONFLATED,
+    onUndeliveredElement = {
+      logx("onUndeliveredElement: $it")
+    })
+
+  val channel1 = produce {
+    (1..3).forEach {
+      send(it)
+      logx("Send: $it")
+    }
+  }
+
+  channel1.consumeEach {
+    logx("Receive: $it")
+  }
+
+  logx("end")
+}
+
 val mySingleDispatcher = Executors.newSingleThreadExecutor {
   Thread(it, "MySingleThread").apply { isDaemon = true }
 }.asCoroutineDispatcher()
-
 @OptIn(ExperimentalStdlibApi::class)
-fun main() = runBlocking(mySingleDispatcher) {
+fun coroutineContextSample() = runBlocking(mySingleDispatcher) {
   /*val user = getUserInfo(this)
   logx(user)*/
-  
+
   val scope = CoroutineScope(Job() + mySingleDispatcher)
-  scope.launch(CoroutineName("FirstCoroutine")) { 
+  scope.launch(CoroutineName("FirstCoroutine")) {
     logx(coroutineContext[CoroutineDispatcher] == mySingleDispatcher)
     delay(1000)
     logx("First end")
   }
-  
+
   delay(500L)
   scope.cancel()
   delay(1000L)
@@ -50,6 +114,20 @@ suspend fun getUserInfo(coroutineScope: CoroutineScope): String {
   return "BoyCoder"
 }
 
+suspend fun coroutineByAsync(coroutineScope: CoroutineScope) {
+  println("In runBlocking: ${Thread.currentThread().name}")
+
+  val deferred: Deferred<String> = coroutineScope.async {
+    println("In async: ${Thread.currentThread().name}")
+    delay(1000)
+    println("after delay")
+    return@async "Async Task completed"
+  }
+  println("After async: ${Thread.currentThread().name}")
+  //println("async result: ${deferred.await()}")
+  delay(2000)
+}
+
 fun Job.log() {
   logx(
     """
@@ -61,13 +139,35 @@ fun Job.log() {
 }
 
 fun logx(any: Any?) {
-  println(
-    """
-==========================
-$any
-Thread: ${Thread.currentThread().name}
-==========================""".trimIndent()
-  )
+  println("$any Thread: ${Thread.currentThread().name}")
+}
+
+private suspend fun coroutineAwaitSample(coroutineScope: CoroutineScope) {
+  suspend fun getResult1(): String {
+    delay(1000L)
+    return "Result1"
+  }
+
+  suspend fun getResult2(): String {
+    delay(1000L)
+    return "Result2"
+  }
+
+  suspend fun getResult3(): String {
+    delay(1000L)
+    return "Result3"
+  }
+
+  var results: List<String>
+  val time = measureTimeMillis {
+    val result1 = coroutineScope.async { getResult1() }
+    val result2 = coroutineScope.async { getResult2() }
+    val result3 = coroutineScope.async { getResult3() }
+
+    results = listOf(result1.await(), result2.await(), result3.await())
+  }
+  println("Time: $time")
+  println("results: $results")
 }
 
 private suspend fun joinApiSample(coroutineScope: CoroutineScope) {
@@ -93,31 +193,8 @@ private suspend fun joinApiSample(coroutineScope: CoroutineScope) {
   logx("Process end")
 }
 
-private suspend fun coroutineAwaitSample(coroutineScope: CoroutineScope) {
-  suspend fun getResult1(): String {
-    delay(1000L)
-    return "Result1"
+fun coroutineByLaunch () {
+  GlobalScope.launch(Dispatchers.IO) {
+    println("Coroutine started: ${Thread.currentThread().name}")
   }
-
-  suspend fun getResult2(): String {
-    delay(1000L)
-    return "Result2"
-  }
-
-  suspend fun getResult3(): String {
-    delay(1000L)
-    return "Result3"
-  }
-
-  var results: List<String>
-  val time = measureTimeMillis {
-
-    val result1 = coroutineScope.async { getResult1() }
-    val result2 = coroutineScope.async { getResult2() }
-    val result3 = coroutineScope.async { getResult3() }
-
-    results = listOf(result1.await(), result2.await(), result3.await())
-  }
-  println("Time: $time")
-  println("results: $results")
 }
--- SwordDemoLibrary/compose/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ SwordDemoLibrary/compose/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.KeyframesSpec
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.TweenSpec
 import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -22,7 +28,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.jetpackcompose.ui.theme.SwordDemoLibraryTheme
 
 
 /*val LocalName = compositionLocalOf<String> {
@@ -30,50 +35,111 @@ import com.example.jetpackcompose.ui.theme.SwordDemoLibraryTheme
 }*/
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    setContent {
+        setContent {
 
-      /*CompositionLocalProvider(LocalName provides "sword") {
-      }*/
-    }
-  }
-
-  @Composable
-  fun animateDpAsStateSample() {
-    var big by remember { mutableStateOf(false) }
-    val anim = animateDpAsState(
-      if (big) 48.dp else 96.dp,
-      spring(Spring.DampingRatioMediumBouncy),
-      label = "")
-    Box(
-      Modifier
-        .size(anim.value)
-        .background(Color.Green)
-        .clickable {
-          big = !big
+            /*CompositionLocalProvider(LocalName provides "sword") {
+            }*/
         }
-    )
-  }
-
-  @Composable
-  fun AnimatableSample() {
-    var big by remember { mutableStateOf(false) }
-    val targetValue = remember(big) { if (big) 96.dp else 48.dp }
-    val animable = remember { Animatable(targetValue, Dp.VectorConverter) }
-
-    //启动动画
-    LaunchedEffect(targetValue) {
-      animable.animateTo(targetValue, spring(Spring.DampingRatioMediumBouncy))
     }
-    Box(
-      Modifier
-        .size(animable.value)
-        .background(Color.Green)
-        .clickable {
-          big = !big
+
+    @Composable
+    fun animateDpAsStateSample() {
+        var big by remember { mutableStateOf(false) }
+        val anim = animateDpAsState(
+            if (big) 48.dp else 96.dp,
+            spring(Spring.DampingRatioMediumBouncy),
+            label = ""
+        )
+        Box(
+            Modifier
+                .size(anim.value)
+                .background(Color.Green)
+                .clickable {
+                    big = !big
+                }
+        )
+    }
+
+    @Composable
+    fun AnimatableSample() {
+        var start by remember { mutableStateOf(false) }
+        val targetValue = remember(start) { if (start) 0.dp else 200.dp }
+        val animable = remember { Animatable(targetValue, Dp.VectorConverter) }
+
+        //启动动画
+        LaunchedEffect(targetValue) {
+            animable.animateTo(targetValue, spring(Spring.DampingRatioMediumBouncy))
         }
-    )
-  }
+        Box(
+            Modifier
+                .size(animable.value)
+                .background(Color.Green)
+                .clickable {
+                    start = !start
+                }
+        )
+    }
+
+    @Composable
+    fun TweenSpecSample() {
+        var big by remember { mutableStateOf(false) }
+        val targetValue = remember(big) { if (big) 96.dp else 48.dp }
+        val animable = remember { Animatable(targetValue, Dp.VectorConverter) }
+
+        //启动动画
+        LaunchedEffect(targetValue) {
+            animable.animateTo(targetValue, TweenSpec(easing = FastOutSlowInEasing))
+        }
+        Box(
+            Modifier
+                .size(48.dp)
+                .offset(animable.value, animable.value)
+                .background(Color.Green)
+                .clickable {
+                    big = !big
+                }
+        )
+    }
+
+    @Composable
+    fun KeyframesSpecSample() {
+        var start by remember { mutableStateOf(false) }
+        val targetValue = remember(start) { if (start) 0.dp else 200.dp }
+        val animable = remember { Animatable(targetValue, Dp.VectorConverter) }
+
+        //启动动画
+        LaunchedEffect(targetValue) {
+            animable.animateTo(
+                targetValue,
+                keyframes {
+                    durationMillis = 450 //设置动画的时长
+                    delayMillis = 500 //设置延时
+                    144.dp at 150 with FastOutSlowInEasing //设置 150ms - 300ms 之间速度曲线
+                    20.dp at 300
+                }
+            )
+        }
+        Box(
+            Modifier
+                .size(animable.value)
+                .background(Color.Green)
+                .clickable {
+                    start = !start
+                }
+        )
+    }
+
+    @Composable
+    fun SpringSpecSample() {
+        var big by remember { mutableStateOf(false) }
+        val targetValue = remember(big) { if (big) 96.dp else 48.dp }
+        val animable = remember { Animatable(targetValue, Dp.VectorConverter) }
+
+        LaunchedEffect(key1 = targetValue) {
+            animable.animateTo(targetValue, SpringSpec())
+        }
+    }
 }
