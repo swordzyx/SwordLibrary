00857db9b904e2b336595b4f335983c01251c43e
sword <35715272+wanderingsword@users.noreply.github.com>
2024-02-27 21:46:54 +0800

    移植 AndroidProject 闪屏页

---
+++ SwordDemoLibrary/app/src/main/java/sword/SplashActivity.kt
@@ -0,0 +1,11 @@
+package sword
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+}
