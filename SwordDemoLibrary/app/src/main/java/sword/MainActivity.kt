272dc5892d5a1085b7d71e56e3cd6d555ca943bf
yanxia.zhou <yanxia.zhou@xinsuanyiti.com>
2024-06-12 13:59:52 +0800

    移除无用代码

--- SwordDemoLibrary/app/src/main/java/sword/MainActivity.kt
+++ SwordDemoLibrary/app/src/main/java/sword/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Base64
-import android.util.Log
 import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
