568fadbadbbe240ba8ce07ed75e6d9cd537304e4
yanxia.zhou <yanxia.zhou@xinsuanyiti.com>
2024-09-23 15:00:12 +0800

    调整注释

--- SwordDemoLibrary/app/src/main/java/sword/InetAddressUtil.java
+++ SwordDemoLibrary/app/src/main/java/sword/InetAddressUtil.java
@@ -3,7 +3,7 @@ package sword;
 import java.net.InetAddress;
 
 /**
- * [InetAddress](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/net/InetAddress.html) 类的用法
+ * [InetAddress](<a href="https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/net/InetAddress.html">Java InetAddress</a>) 类的用法
  */
 public class InetAddressUtil {
     public String getIP() {
