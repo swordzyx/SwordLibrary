710deae7c702c551ef9c5c6007da984e11198001
sword <1163201968@qq.com>
2024-11-14 00:45:51 +0800

    移除测试相关的代码目录

--- SwordLibrary/kotlinsample/build.gradle.kts
+++ SwordLibrary/kotlinsample/build.gradle.kts
@@ -39,11 +39,7 @@ android {
 }
 
 dependencies {
-
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
