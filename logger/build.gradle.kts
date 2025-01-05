11fc690b67c2612201a07af1a671739ad68c2083
zhouyanxia <1163201968@qq.com>
2025-01-06 00:00:21 +0800

    移除无用的依赖

--- logger/build.gradle.kts
+++ logger/build.gradle.kts
@@ -33,11 +33,5 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    implementation(libs.kotlinx.coroutines.core)
 }
