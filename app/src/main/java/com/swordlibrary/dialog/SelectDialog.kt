a9537144fb45d079fe71780cfdc906f8fd93f9d0
yanxia.zhou <zhouyanxia@xinsuanyiti.com>
2025-05-28 14:55:40 +0800

    移除无用代码和文件

--- app/src/main/java/com/swordlibrary/dialog/SelectDialog.kt
+++
@@ -1,9 +0,0 @@
-package com.swordlibrary.dialog
-
-/**
- * 参考：https://github.com/getActivity/AndroidProject-Kotlin
- * todo: Dialog 组件库
- */
-class SelectDialog {
-
-}
--- app/src/main/java/com/swordlibrary/restart/PhoenixActivity.kt
+++ app/src/main/java/com/swordlibrary/restart/PhoenixActivity.kt
@@ -6,6 +6,5 @@ import android.os.Bundle
 class PhoenixActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 }
